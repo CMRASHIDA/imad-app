1b8b1871c0027134bd6ab46ffacd46a79aa8e44a
CMRASHIDA <rcmrashi@gmail.com>
2017-08-20 16:47:37 +0430

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
 var img=document.getElementById('rashi');
-
+var marginLeft=0;
 function moveRight()
 {
-    var marginLeft=0;
+    
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
